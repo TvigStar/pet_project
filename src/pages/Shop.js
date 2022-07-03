30f9f1b778558c87e566208c37194ed761f99af3
TvigStar <kolesnykproduction@gmail.com>
2022-07-03 12:03:57 +0300

    login from backend

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,8 +1,27 @@
 import product from '../content/product1.jpg'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {ProductCreate} from "../components";
+import {Api} from "../api";
 
 export const Shop = () => {
+
+    const [data, setData] = useState([])
+    const fetchProducst = async () => {
+        const fetchdata = await Api.product.getAll()
+        setData(fetchdata.data)
+        console.log(fetchdata)
+    }
+
+    useEffect(() => {
+        fetchProducst()
+    }, [])
+
+    console.log(data)
+    const product = data.map( value => {
+
+    })
+    console.log(product)
+
     const [newProduct, setNewProduct] = useState(false)
     const handleNewProduct = () => {
         setNewProduct(true)
@@ -10,7 +29,9 @@ export const Shop = () => {
 
     if (newProduct) {
         return (
-                <ProductCreate handleClose={()=>{setNewProduct(false)}} />
+            <ProductCreate handleClose={() => {
+                setNewProduct(false)
+            }}/>
         )
     }
     return (
@@ -19,19 +40,22 @@ export const Shop = () => {
                 <button className='sh_bnt_style' onClick={handleNewProduct}> create product</button>
             </div>
 
-            <div className='product_main'>
-                <div className='product_up'>
-                    <img src={product} alt={product}/>
+            {data.map(value => (
+                <div className='product_main'>
+                    <div className='product_up'>
+                        {/*<img src={product} alt={product}/>*/}
+                    </div>
+                    <div className='product_down'>
+                        <span>Title: {value.title}</span>
+                        <br/>
+                        <span>Price: {value.price}</span>
+                    </div>
+                    <div>
+                        <button> buy</button>
+                    </div>
                 </div>
-                <div className='product_down'>
-                    <span>Title: title</span>
-                    <br/>
-                    <span>Price: price</span>
-                </div>
-                <div>
-                    <button> buy</button>
-                </div>
-            </div>
+            ))}
+
         </div>
     )
 }
